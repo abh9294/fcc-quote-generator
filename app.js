07649ab5920c96a297555a6be428ab3a6c7b4175
abh9294 <abh9294@gmail.com>
2023-07-14 15:19:59 +0530

    activated twitter sharing option , added git ignore

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import quoteCollection from './quotes.js';
 const text = document.querySelector('#main-quote-text');
 const author= document.querySelector('#author');
 const button= document.querySelector('#new-quote');
+const tweetLink= document.querySelector('#tweet-quote');
+
 
 
 const handleClick = () => {
@@ -13,10 +15,19 @@ const handleClick = () => {
     return randomIndex;
 };
 
+const handleTweet=(event) => {
+    event.preventDefault();
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteCollection.quotes[handleClick()].quote)}`;
+
+    window.open(tweetUrl,"_blank")
+};
+
+
+
 text.textContent=quoteCollection.quotes[handleClick()].quote;
 author.textContent=quoteCollection.quotes[handleClick()].author;
 
 
 button.addEventListener('click', handleClick);
-
+tweetLink.addEventListener('click',handleTweet);
 
